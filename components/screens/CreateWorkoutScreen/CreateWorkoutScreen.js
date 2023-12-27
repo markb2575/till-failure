9397a373b28e8dce979ffd9b18e37b09ff6e8a50
RafaelBGirgis <89926956+RafaelBGirgis@users.noreply.github.com>
2023-12-27 13:44:43 -0500

    Update CreateWorkoutScreen.js

--- components/screens/CreateWorkoutScreen/CreateWorkoutScreen.js
+++ components/screens/CreateWorkoutScreen/CreateWorkoutScreen.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { View, Text, Button, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
+import { useState, useEffect, React } from 'react';
+import { View, Text, setText, Button, Image, ScrollView, Dimensions, TouchableOpacity, TextInput} from 'react-native';
 import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FileSystemCommands from "../../util/FileSystemCommands"
@@ -10,6 +10,7 @@ import CustomCard from '../CustomCard';
 export default function CreateWorkoutScreen({ navigation }) {
     const [data, setData] = useState(null);
     const isFocused = useIsFocused();
+    const [inputValue, setText] = useState('');
     useEffect(() => {
         if (isFocused) {
             FileSystemCommands.setupProject().then(res => {
@@ -28,7 +29,24 @@ export default function CreateWorkoutScreen({ navigation }) {
                                     <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', margin: 5}}>Back</Text>
                                 </View>
                             </TouchableOpacity>
-                        } />
+            } />
+            <CustomCard marginRight={0} marginTop={0} screen ={
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                                <TextInput style={{fontSize:22, padding:20, marginHorizontal:16.25, color: 'white', margin: 5 }}
+                                    placeholder="Enter Workout Name"
+                                    placeholderTextColor="#d3d3d3"
+                                    value={inputValue}
+                                    onChangeText={newText => setText(newText)}
+                                />
+                            </View>               
+            } />
+            {/* <CustomCard marginRight={0} screen={
+                            <TouchableOpacity onPress={() => navigation.navigate("Programs")} style={{padding:10, marginHorizontal:16.25}}>
+                                <View style={{ flexDirection: 'row', alignItems: 'bottom', justifyContent: 'center'}}>
+                                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', margin: 5}}>Save</Text>
+                                </View>
+                            </TouchableOpacity>
+                        } /> */}
         </View>
     );
 }
