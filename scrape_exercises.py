770729d35655dd9199c6352834c54acdd4fc3f1e
markb2575 <markbassily2575@gmail.com>
2024-01-09 18:54:34 -0500

    decided to switch workouts

--- scrape_exercises.py
+++ scrape_exercises.py
@@ -2,63 +2,21 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-# url = "https://fitnessprogramer.com/exercises/page/"
 num = 1
 with open("exercises.json" , "w") as f:
-    f.write('    "workouts": [')
+    f.write('"workouts": [\n')
     while (True):
-        html = requests.get(f"https://fitnessprogramer.com/exercises/page/{num}").text
-        if (html.find("No Results Found") != -1):
+        html = requests.get(f"https://www.bodybuilding.com/exercises/finder/{num}/?exercise-type=strength&equipment=bands,barbell,kettlebells,machine,cable,dumbbell,ez-curl-bar").text
+        if (html.find("No exercises were found that matched the selected options.") != -1):
             break
         soup = BeautifulSoup(html, 'html.parser')
-        entries = soup.find_all("article", class_="entry")
+        entries = soup.find_all("div", class_="ExResult-row flexo-container flexo-between")
         for entry in entries:
-            try:
-                main_info = entry.find("div", class_="main_info")
-
-                # Extracting exercise name
-                exercise_name = main_info.find("h2", class_="title").get_text(strip=True)
-
-                # Extracting equipment_required
-                equipment_label = main_info.find("div", class_="exercise_meta equipment").find("label").get_text(strip=True)
-                equipment_value = main_info.find("div", class_="exercise_meta equipment").get_text(strip=True, separator=' ')
-                equipment_required = "true"
-                if (equipment_value.find("NO EQUIPMENT") != -1): equipment_required = "false"
-
-                # Extracting primary muscles
-                muscles_value = main_info.find("div", class_="exercise_meta primary_muscles")
-                if (muscles_value == None): continue
-                muscles_value = muscles_value.get_text(strip=True, separator=' ').replace("Calisthenic","").replace("Neck","").replace("Erector Spinae","").replace("Full Body","").replace("Abs / Core","Abs").replace("Back / Wing","Back").replace("Primary Muscles: ", '"').replace(", ",'", "') + '"'
-                muscles_value = muscles_value.replace(', ""',"").replace('"",',"").replace('""',"")
-                if (muscles_value == "" or muscles_value.find("Cardio") != -1 or muscles_value.find("Yoga") != -1): continue
-                print(f'{{"name": "{exercise_name}", "targets": [{muscles_value}], "equipment_required": {equipment_required}, "data": []}}, {num}')
-                f.write(f'{{"name": "{exercise_name}", "targets": [{muscles_value}], "equipment_required": {equipment_required}, "data": []}},\n')
-            except:
-                continue
+            name_muscle = entry.find("div", class_="ExResult-cell ExResult-cell--nameEtc")
+            name = name_muscle.find("h3", class_="ExHeading ExResult-resultsHeading").get_text(strip=True)
+            muscle = name_muscle.find("div", class_="ExResult-details ExResult-muscleTargeted").get_text(strip=True).replace("Muscle Targeted:","")
+            rating = entry.find("div", class_="ExResult-cell ExResult-cell--rating").find("div",class_="ExRating-badge").get_text(strip=True)
+            if rating == "n/a": continue
+            f.write(f'    {{"name": "{name}", "target": "{muscle}", "rating": {rating}, "data": []}},\n')
         num += 1
-    f.write("    ],")
-    # time.sleep(2)
-        
-# html = requests.get(f"https://fitnessprogramer.com/exercises/page/{num}").text
-# muscles = {}
-# soup = BeautifulSoup(html, 'html.parser')
-# entries = soup.find_all("article", class_="entry")
-
-# for entry in entries:
-    # main_info = entry.find("div", class_="main_info")
-
-    # # Extracting exercise name
-    # exercise_name = main_info.find("h2", class_="title").get_text(strip=True)
-
-    # # Extracting equipment_required
-    # equipment_label = main_info.find("div", class_="exercise_meta equipment").find("label").get_text(strip=True)
-    # equipment_value = main_info.find("div", class_="exercise_meta equipment").get_text(strip=True, separator=' ')
-    # equipment_required = "true"
-    # if (equipment_value.find("NO EQUIPMENT") != -1): equipment_required = "false"
-
-    # # Extracting primary muscles
-    # muscles_value = main_info.find("div", class_="exercise_meta primary_muscles").get_text(strip=True, separator=' ').replace("Cardio","").replace("Erector Spinae","").replace("Full Body","").replace("Abs / Core","Abs").replace("Back / Wing","Back").replace("Primary Muscles: ", '"').replace(", ",'", "') + '"'
-    # muscles_value = muscles_value.replace(', ""',"").replace('"",',"").replace('""',"")
-    # if (muscles_value == ""): continue
-
-    # print(f'{{"name": "{exercise_name}", "targets": [{muscles_value}], "equipment_required": {equipment_required}, "data": []}},')
+    f.write("],")
